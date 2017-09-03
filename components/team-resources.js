fc8bb0839b9aedbb757a46177cd052be0f1f2e31
SunooBertsch <sbertsch92@yahoo.com>
2017-09-03 14:48:26 -0700

    (trivial) fixed typos and changed wording per clients request

--- components/team-resources.js
+++ components/team-resources.js
@@ -1,58 +1,63 @@
 module.exports = (app, Telegraf) => {
-	/*
+  /*
 	* 'Welcome To The Team 🤗' reply
 	** @DIRECT TO 'TEAM RESOURCES'
 	*/
-	app.hears("1) Welcome 2 Team❗️", ctx => {
-		return ctx.reply("Team Resources", teamResources).then();
-	});
+  app.hears("Welcome 2 Team❗️", ctx => {
+    return ctx
+      .reply(
+        "Welcome to the team❗️\n\nGo through the buttons below to make sure you’re connected to everything WorldVentures...",
+        teamResources
+      )
+      .then();
+  });
 
-	/*********************
+  /*********************
   Team Resources Menu
 	@teamchats, teamapps, facebook groups, home
 	*********************/
 
-	const teamResources = Telegraf.Extra
-		.markdown()
-		.markup(m =>
-			m
-				.keyboard([
-					[m.callbackButton("Team Chats 👫"), m.callbackButton("Team Apps 📱")],
-					[m.callbackButton("Facebook Groups 🌎"), m.callbackButton("Home 🏠")]
-				])
-				.resize()
-		);
+  const teamResources = Telegraf.Extra
+    .markdown()
+    .markup(m =>
+      m
+        .keyboard([
+          [m.callbackButton("Team Chats 👫"), m.callbackButton("Team Apps 📱")],
+          [m.callbackButton("Facebook Groups 🌎"), m.callbackButton("Home 🏠")]
+        ])
+        .resize()
+    );
 
-	/*
+  /*
 	* 'Team Chats' reply
 	*/
-	app.hears("Team Chats 👫", ctx => {
-		return ctx
-			.reply(
-				"Telegram is the number one way the team maintains communication with each other via the channel and chat below…\n\n💠 Team Unstoppable Channel 💠\n Big training events, team calls, team bonding, rank recognition will be announced here. Pay attention to this page as much as possible.\n\n✅ Add business reps ONLY to this channel!\nClick the link below to join now!\n t.me/UnstoppableChannel\n\n💠 Team Unstoppable Chat 💠 \n A group chat to stay plugged in to our team members from around the world! Welcome your new teammates, post travel parties, and recognize new ranks here too.\n\n ✅ Add business reps ONLY to this chat! \n Click the link below to join now! \nt.me/UnstoppableGroupChat"
-			)
-			.then();
-	});
+  app.hears("Team Chats 👫", ctx => {
+    return ctx
+      .reply(
+        "Telegram is the number one way the team maintains communication with each other via the channel and chat below…\n\n💠 Team Unstoppable Channel 💠\n Big training events, team calls, team bonding, rank recognition will be announced here. Pay attention to this page as much as possible.\n\n✅ Add business reps ONLY to this channel!\nClick the link below to join now!\n t.me/UnstoppableChannel\n\n💠 Team Unstoppable Chat 💠 \n A group chat to stay plugged in to our team members from around the world! Welcome your new teammates, post travel parties, and recognize new ranks here too.\n\n ✅ Add business reps ONLY to this chat! \n Click the link below to join now! \nt.me/UnstoppableGroupChat"
+      )
+      .then();
+  });
 
-	/*
+  /*
 	* 'Team Apps' reply
 	*/
-	app.hears("Team Apps 📱", ctx => {
-		return ctx
-			.reply(
-				"💠 Dreamtrips 💠\nUser ID and password have been emailed to you. Set up your profile picture and start booking your first DreamTrip!\n\n💠 ChillinWW (ChillinWorldWide) 💠\nThis app has EVERYTHING you need for success in WorldVentures aside from your mentors! Plug in to the QuickCoach videos, Flye SmartCard (password: boomer1), or Audios from any of our top leaders."
-			)
-			.then();
-	});
+  app.hears("Team Apps 📱", ctx => {
+    return ctx
+      .reply(
+        "💠 Dreamtrips 💠\nUser ID and password have been emailed to you. Set up your profile picture and start booking your first DreamTrip!\n\n💠 ChillinWW (ChillinWorldWide) 💠\nThis app has EVERYTHING you need for success in WorldVentures aside from your mentors! Plug in to the QuickCoach videos, Flye SmartCard (password: boomer1), or Audios from any of our top leaders."
+      )
+      .then();
+  });
 
-	/*
+  /*
 	* 'Facebook Groups' reply
 	*/
-	app.hears("Facebook Groups 🌎", ctx => {
-		return ctx
-			.reply(
-				"💠 Team Unstoppable FB Group 💠 \nThe main Facebook page where we connect with ALL Unstoppable teammates and where IMDs Tere and Mary Kampe and other leaders welcome new teammates and post about huge company updates and events. \n\n ✅ Add business reps ONLY to this group! \n\n 💠 Funstoppable FB Group 💠 \nWhere all Unstoppable Dreamtrips members come together and share stories about their experiences with all things DreamTrips (trip, DreamTrips Local, Price Pledge, etc. testimonials). This is not a Facebook page about business, so no “Welcome to the Team” posts or anything business related here!\n\n✅ Add EVERYONE to this group!"
-			)
-			.then();
-	});
+  app.hears("Facebook Groups 🌎", ctx => {
+    return ctx
+      .reply(
+        "💠 Team Unstoppable FB Group 💠 \nThe main Facebook page where we connect with ALL Unstoppable teammates and where IMDs Tere and Mary Kampe and other leaders welcome new teammates and post about huge company updates and events. \n\n ✅ Add business reps ONLY to this group! \n\n 💠 Funstoppable FB Group 💠 \nWhere all Unstoppable Dreamtrips members come together and share stories about their experiences with all things DreamTrips (trip, DreamTrips Local, Price Pledge, etc. testimonials). This is not a Facebook page about business, so no “Welcome to the Team” posts or anything business related here!\n\n✅ Add EVERYONE to this group!"
+      )
+      .then();
+  });
 };
--- components/training.js
+++ components/training.js
@@ -1,47 +1,47 @@
 module.exports = (app, Telegraf) => {
-	/*
+  /*
 	* 'Training 🥊' reply
 	** @DIRECT TO 'TRAINING'
 	*/
-	app.hears("2) Training 🥊", ctx => {
-		return ctx.reply("Training", training).then();
-	});
+  app.hears("Training 🥊", ctx => {
+    return ctx.reply("Training", training).then();
+  });
 
-	/*********************
+  /*********************
 	  TRAINING Menu
 	@teamchats, teamapps, facebook groups, home
 	*********************/
 
-	const training = Telegraf.Extra
-		.markdown()
-		.markup(m =>
-			m
-				.keyboard([
-					[m.callbackButton("Events 🏋️"), m.callbackButton("Audios 🎧")],
-					[m.callbackButton("Home 🏠")]
-				])
-				.resize()
-		);
+  const training = Telegraf.Extra
+    .markdown()
+    .markup(m =>
+      m
+        .keyboard([
+          [m.callbackButton("Events 🏋️"), m.callbackButton("Audios 🎧")],
+          [m.callbackButton("Home 🏠")]
+        ])
+        .resize()
+    );
 
-	/*
+  /*
 	* 'Events' reply
 	*/
-	app.hears("Events 🏋️", ctx => {
-		return ctx
-			.reply(
-				"Learners are earners!\n\n Training events are a HUGE piece to the WV success formula, and the best part is we’re learning from the best of the best in our company who teach us EXACTLY how they reached the top ranks in the company. There has never been a rep that has had big, long-lasting success in this company without trainings.\n\nRegister for the next training event now before it sells out!\nwveventreg.com\nwveventreg.com\nwveventreg.com"
-			)
-			.then();
-	});
+  app.hears("Events 🏋️", ctx => {
+    return ctx
+      .reply(
+        "Learners are earners!\n\nTraining events are a HUGE piece to the WV success formula, and the best part is we’re learning from the best of the best in our company who teach us EXACTLY how they reached the top ranks in the company. There has never been a rep that has had big, long-lasting success in this company without trainings.\n\nRegister for the next training event now before it sells out!\nwveventreg.com\nwveventreg.com\nwveventreg.com"
+      )
+      .then();
+  });
 
-	/*
+  /*
 	* 'Audios' reply
 	*/
-	app.hears("Audios 🎧", ctx => {
-		return ctx
-			.reply(
-				"Start feeding your brain before one of the big events by listening to audios in the...\n\n💠 ChillinWW App 💠\nHead over to the “Audios” section in the ChillinWW App for FREE audio experience by your Top Leaders in WV!\n\n💠 Advantage Lite 💠\nTake your listening to the next level!! For only $5.99/mo, you’ll gain access to a handful of audios by our top leaders, the E-Version of our recent Voyager Magazines, AND be invited to The Secret Advantage FB Group---where top leaders go LIVE!!\n\n✅ Sign-up in your BackOffice (domain.worldventures.biz)\n\n💠 Advantage 💠\nThe more you invest, the more you’ll gain. Unlock the entire WV Library of Audios and Video by your top leaders & amazing speakers from around the world! For $19.99/mo, you’ll also have 3 Audio CD’s, Voyager, and Success Magazines mailed out to you. Worth it!\n\n✅ Sign-up in your BackOffice (domain.worldventures.biz)"
-			)
-			.then();
-	});
+  app.hears("Audios 🎧", ctx => {
+    return ctx
+      .reply(
+        "Start feeding your brain before one of the big events by listening to audios in the...\n\n💠 ChillinWW App 💠\nHead over to the “Audios” section in the ChillinWW App for FREE audio experience by your Top Leaders in WV!\n\n💠 Advantage Lite 💠\nTake your listening to the next level!! For only $5.99/mo, you’ll gain access to a handful of audios by our top leaders, the E-Version of our recent Voyager Magazines, AND be invited to The Secret Advantage FB Group---where top leaders go LIVE!!\n\n✅ Sign-up in your BackOffice (domain.worldventures.biz)\n\n💠 Advantage 💠\nThe more you invest, the more you’ll gain. Unlock the entire WV Library of Audios and Video by your top leaders & amazing speakers from around the world! For $19.99/mo, you’ll also have 3 Audio CD’s, Voyager, and Success Magazines mailed out to you. Worth it!\n\n✅ Sign-up in your BackOffice (domain.worldventures.biz)"
+      )
+      .then();
+  });
 };
